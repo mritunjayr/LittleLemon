55347d6f873e68841b16b5763e7e33c391ac8e30
Mritunjay Kumar <mritunjaykumar694@gmail.com>
2023-02-27 01:40:51 +0530

    adding serializers and created rest api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import  ModelViewSet
+from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu, Booking
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(ListCreateAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+
+class BookingViewSet(ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
